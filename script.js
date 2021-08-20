5b49ae9844d91d39987667d0113d4f6267f488b3
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-20 16:03:54 -0500

    base html and base math function

---
+++ script.js
@@ -0,0 +1,35 @@
+//base math functions
+
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+//operate function that calls base functions
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return sum(a, b);
+    }
+    if (operator === '-') {
+        return subtract(a, b);
+    }
+    if (operator === '*') {
+        return multiply(a, b);
+    }
+    if (operator === '/') {
+        return divide(a, b);
+    }
+
+}
